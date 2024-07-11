6c9c07126e65455c8a6ee46a5bde3e98524ae65d
akradadiya28 <akradadiya28@gmail.com>
2024-07-11 16:00:10 +0530

    Dog API

--- js/script.js
+++ js/script.js
@@ -1,37 +1,37 @@
 let itemList = document.getElementById("itemList");
+let img = document.getElementById("img");
 
 const breedList = () => {
     fetch('https://dog.ceo/api/breeds/list/all')
         .then(res => res.json())
         .then(list => {
-            console.log("response", list);
-            const data = () => {
-                let listMsg = list.message;
+            // console.log("response", list);
 
-                for (const key in listMsg) {
-                    console.log(key);
-                    if (listMsg[key].length === 0) {
-                        // console.log(key);
-                        itemList.innerHTML += `<li class="item-list">
+            let listMsg = list.message;
+
+            for (const key in listMsg) {
+                // console.log(key);
+                if (listMsg[key].length === 0) {
+                    // console.log(key);
+                    itemList.innerHTML += `<li class="item-list" onclick="return imageList('${key}')">
                                     <a href="#" class="act">
-                                        <span>${[key]}</span>
+                                        <span>${key}</span>
                                     </a>
                             </li>`;
-                    } else {
-                        let subList = `<ul class="dropdown-menu">`;
-                        listMsg[key].forEach(sub => {
-                            subList += `<li>
-                                    <a href="javascript:void(0)" class="">
+                } else {
+                    let subList = `<ul class="dropdown-menu">`;
+                    listMsg[key].forEach(sub => {
+                        subList += `<li>
+                                    <a href="javascript:void(0)">
                                         <span>${sub}</span>
                                     </a>
                                 </li>`
-                        });
-                        subList += `</ul>`;
-                        itemList.innerHTML += `<li class="item-list"><a href="javascript:void(0)" class="dropdown pos-relative act">${key}</a> ${subList}</li>`
-                    }
+                    });
+                    subList += `</ul>`;
+                    itemList.innerHTML += `<li class="item-list" onclick="return imageList('${key}')"><a href="javascript:void(0)" class="dropdown pos-relative act">${key}</a> ${subList}</li>`
                 }
-            };
-            data();
+            }
+
         })
         .catch((err) => {
             console.log("Error 404", err);
@@ -39,4 +39,23 @@ const breedList = () => {
 };
 
 // Call the function to fetch and display the list
-breedList()
+breedList();
+
+const imageList = (breed) => {
+    fetch(`https://dog.ceo/api/breed/${breed}/images`)
+        .then(res => res.json())
+        .then((images) => {
+            const imgList = images.message;
+            img.innerHTML = ""; // Clear the existing content
+
+            imgList.forEach((imageUrl) => {
+                img.innerHTML += `<div class="w-3"
+                            style="margin: 5px; max-width: 100%; max-height:100%;object-fit:cover;">
+                        <img src="${imageUrl}" alt="" width="100%" height="100%">
+                        </div>`;
+            });
+        })
+        .catch((err) => {
+            console.log("Error", err);
+        });
+};
